a3c4af4a1e3881603ee0bbd6137e66988b2aed9f
qre100g <kmnrddy4334@gmail.com>
2026-02-03 22:28:44 +0530

    Created a new MacOS framework with FeedItem

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeed.swift
+//  EssentialFeed
+//
+//  Created by Mukesh Kondreddy on 03/02/26.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
