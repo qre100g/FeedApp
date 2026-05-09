2945339c13f5d619ef17c78f93c49d28bcc05d26
Mukesh <kmnrddy4334@gmail.com>
2026-05-10 01:23:26 +0530

    Created a new project

---
+++ FeedApp/FeedApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FeedApp
+//
+//  Created by Mukesh Kondreddy on 10/05/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
