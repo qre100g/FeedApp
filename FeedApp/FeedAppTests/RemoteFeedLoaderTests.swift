3d33fbbc725c9f3499a8119281bf5c0b81dc9e95
Mukesh <kmnrddy4334@gmail.com>
2026-05-10 01:43:42 +0530

    Make HTTPClinet a protocol to inject it as it doesn't have to be a single ton.

--- FeedApp/FeedAppTests/RemoteFeedLoaderTests.swift
+++ FeedApp/FeedAppTests/RemoteFeedLoaderTests.swift
@@ -8,31 +8,41 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://example.com")
+        client.get(from: URL(string: "https://example.com")!)
     }
 }
 
-class HTTPClient {
-    static let shared = HTTPClient()
-    
-    private init() {}
+protocol HTTPClient {
+    func get(from url: URL)
+}
 
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestFeed() {
-        let client = HTTPClient.shared
-        _ = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestsDataFromURL() {
-        let client = HTTPClient.shared
-        let sut = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
         
         sut.load()
         
