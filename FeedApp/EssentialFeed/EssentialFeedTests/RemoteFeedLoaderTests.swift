614356018d88cba3762db2fbd8fc2d281040330a
qre100g <kmnrddy4334@gmail.com>
2026-05-20 01:01:01 +0530

    Verify for sample codes that the load command will deliver the errors.

--- FeedApp/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ FeedApp/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -39,13 +39,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnNon200HTTPClientResponse() {
         let (sut, client) = makeSUT()
-        
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load() { capturedErrors.append($0) }
 
-        client.complete(withCode: 400)
-        
-        XCTAssertEqual(capturedErrors, [.invalidData])
+        let samples = [199, 201, 300, 400, 500]
+        samples.enumerated().forEach { index, code in
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load() { capturedErrors.append($0) }
+            client.complete(withCode: 400, at: index)
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
     
     // MARK: - Private Helpers
