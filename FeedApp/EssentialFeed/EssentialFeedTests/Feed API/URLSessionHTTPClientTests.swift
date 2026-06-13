2b16b262251b3ad8c66f01f82aae1c8a84fee492
Mukesh <kmnrddy4334@gmail.com>
2026-06-14 01:17:14 +0530

    Verify that the getFromURL perform get request with correct URL

--- FeedApp/EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ FeedApp/EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -26,9 +26,29 @@ class URLSessionHTTPClient {
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
+    
+    func test_getFromURL_performsGETRequestWithURL() {
+        URLProtocolSpy.startInterceptingNetwork()
+        
+        let url = URL(string: "https://any-url.com")!
+        let sut = URLSessionHTTPClient()
+        
+        let exp = expectation(description: "Wait for completion")
+        URLProtocolSpy.observeRequests { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            
+            exp.fulfill()
+        }
+        
+        sut.get(from: url) { _ in }
+        
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolSpy.stopInterceptingNetwork()
+    }
 
     func test_getFromURL_failsOnRequestError() {
-        URLProtocol.registerClass(URLProtocolSpy.self)
+        URLProtocolSpy.startInterceptingNetwork()
         let sut = URLSessionHTTPClient()
         let url = URL(string: "https://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
@@ -49,7 +69,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
-        URLProtocol.unregisterClass(URLProtocolSpy.self)
+        URLProtocolSpy.stopInterceptingNetwork()
     }
     
     // MARK: - Helpers
@@ -58,6 +78,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     private class URLProtocolSpy: URLProtocol {
         
         private static var stub: Stub?
+        private static var requestObserver: ((URLRequest) -> Void)?
         
         private struct Stub {
             let data: Data?
@@ -69,7 +90,22 @@ final class URLSessionHTTPClientTests: XCTestCase {
             stub = Stub(data: data, response: response, error: error)
         }
         
+        static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+            requestObserver = observer
+        }
+        
+        static func startInterceptingNetwork() {
+            URLProtocol.registerClass(URLProtocolSpy.self)
+        }
+        
+        static func stopInterceptingNetwork() {
+            URLProtocol.unregisterClass(URLProtocolSpy.self)
+            stub = nil
+            requestObserver = nil
+        }
+        
         override class func canInit(with request: URLRequest) -> Bool {
+            requestObserver?(request)
             return true
         }
         
