6b0023ba9f21fe62b314445e817a7cc39b4963e8
Mukesh <kmnrddy4334@gmail.com>
2026-06-14 14:26:08 +0530

    Track memory leaks for the SUT

--- FeedApp/EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ FeedApp/EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -99,8 +99,20 @@ final class URLSessionHTTPClientTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT() -> URLSessionHTTPClient {
-        return URLSessionHTTPClient()
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
+        let sut = URLSessionHTTPClient()
+        trackMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func trackMemoryLeaks(
+        _ instance: AnyObject,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should be deallocated", file: file, line: line)
+        }
     }
     
     private func anyURL() -> URL {
