ba7b9d4029460ac9ccc481b045b003a01eb4649b
Mukesh <kmnrddy4334@gmail.com>
2026-06-18 23:55:52 +0530

    Add memory leak tracking for the SUT and FeedStore

--- FeedApp/EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedLoaderTests.swift
+++ FeedApp/EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedLoaderTests.swift
@@ -47,9 +47,11 @@ class LocalFeedLoaderTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
+        trackMemoryLeaks(sut, file: file, line: line)
+        trackMemoryLeaks(store, file: file, line: line)
         return (sut, store)
     }
     
