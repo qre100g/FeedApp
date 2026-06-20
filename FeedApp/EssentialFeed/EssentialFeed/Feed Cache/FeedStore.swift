ac664a1570d834811e4d4545acc3c7d940992f6e
Mukesh <kmnrddy4334@gmail.com>
2026-06-21 00:46:47 +0530

    Move LocalFeedItem to its own file

--- FeedApp/EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ FeedApp/EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -19,18 +19,3 @@ public protocol FeedStore {
         completion: @escaping InsertFeedCompletion
     )
 }
-
-public struct LocalFeedItem: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let imageURL: URL
-    
-    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.imageURL = imageURL
-    }
-}
-
