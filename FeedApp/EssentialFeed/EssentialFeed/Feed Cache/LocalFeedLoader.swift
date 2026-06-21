0490395e1c7f3644ab9f2953b9a32b86919004ab
Mukesh <kmnrddy4334@gmail.com>
2026-06-21 15:59:59 +0530

    Segment functionalities into extensions

--- FeedApp/EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ FeedApp/EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -8,17 +8,29 @@
 import Foundation
 
 public class LocalFeedLoader {
-    public typealias SaveResult = Error?
-    public typealias LoadResult = LoadFeedResult
-
+    
     private let store: FeedStore
     private let currentDate: () -> Date
     private let calendar = Calendar(identifier: .gregorian)
-
+    
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
     }
+    
+    private var maxCacheAgeInDays: Int { 7 }
+    
+    private func validate(_ timestamp: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        
+        return currentDate() < maxCacheAge
+    }
+}
+
+extension LocalFeedLoader {
+    public typealias SaveResult = Error?
     
     public func save(_ images: [FeedImage], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed() { [weak self] error in
@@ -32,49 +44,6 @@ public class LocalFeedLoader {
         }
     }
     
-    public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { [weak self] result in
-            guard let self else { return }
-
-            switch result {
-            case let .failure(error):
-                completion(.failure(error))
-
-            case let .found(feed, timestamp) where self.validate(timestamp):
-                completion(.success(feed.toModels()))
-
-            case .found, .empty:
-                completion(.success([]))
-            }
-        }
-    }
-    
-    public func validateCache() {
-        store.retrieve { [weak self] result in
-            guard let self else { return }
-
-            switch result {
-            case .failure:
-                self.store.deleteCachedFeed { _ in }
-
-            case let .found(_, timestamp) where !self.validate(timestamp):
-                self.store.deleteCachedFeed { _ in }
-            
-            case .found, .empty: break
-            }
-        }
-    }
-    
-    private var maxCacheAgeInDays: Int { 7 }
-    
-    private func validate(_ timestamp: Date) -> Bool {
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
-            return false
-        }
-        
-        return currentDate() < maxCacheAge
-    }
-    
     private func insert(_ images: [LocalFeedImage], completion: @escaping (SaveResult) -> Void) {
         store.insert(images, timestamp: currentDate()) { [weak self] error in
             guard self != nil else { return }
@@ -82,6 +51,47 @@ public class LocalFeedLoader {
             completion(error)
         }
     }
+}
+
+extension LocalFeedLoader {
+    public typealias LoadResult = LoadFeedResult
+    
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrieve { [weak self] result in
+            guard let self else { return }
+            
+            switch result {
+            case let .failure(error):
+                completion(.failure(error))
+                
+            case let .found(feed, timestamp) where self.validate(timestamp):
+                completion(.success(feed.toModels()))
+                
+            case .found, .empty:
+                completion(.success([]))
+            }
+        }
+    }
+}
+
+extension LocalFeedLoader {
+    
+    public func validateCache() {
+        store.retrieve { [weak self] result in
+            guard let self else { return }
+            
+            switch result {
+            case .failure:
+                self.store.deleteCachedFeed { _ in }
+                
+            case let .found(_, timestamp) where !self.validate(timestamp):
+                self.store.deleteCachedFeed { _ in }
+                
+            case .found, .empty: break
+            }
+        }
+    }
+    
 }
 
 private extension Array where Element == FeedImage {
