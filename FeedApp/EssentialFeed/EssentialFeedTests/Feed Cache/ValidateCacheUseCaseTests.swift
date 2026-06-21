38c5cd1ca43db23edb8d0f6571313a79d60e4a09
Mukesh <kmnrddy4334@gmail.com>
2026-06-21 15:38:48 +0530

    ValidateCache does not delete less than seven days old cache

--- FeedApp/EssentialFeed/EssentialFeedTests/Feed Cache/ValidateCacheUseCaseTests.swift
+++ FeedApp/EssentialFeed/EssentialFeedTests/Feed Cache/ValidateCacheUseCaseTests.swift
@@ -35,6 +35,18 @@ class ValidateCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.messages, [.retrieve])
     }
     
+    func test_validateCache_doesNotDeleteCacheOnLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        sut.validateCache()
+        store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.messages, [.retrieve])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(
@@ -52,5 +64,29 @@ class ValidateCacheUseCaseTests: XCTestCase {
     private func anyNSError() -> NSError {
         NSError(domain: "any error", code: 1)
     }
+    
+    private func uniqueImage() -> FeedImage {
+        return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+    }
+    
+    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+        let feed = [uniqueImage(), uniqueImage()]
+        let local = feed.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, image: $0.url) }
+        return (feed, local)
+    }
 
+    private func anyURL() -> URL {
+        URL(string: "https://any-url.com")!
+    }
+
+}
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        self + seconds
+    }
 }
