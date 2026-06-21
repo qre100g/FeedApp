1ec107be8ec0718ef2afc6f16b397c3eb2cd9add
Mukesh <kmnrddy4334@gmail.com>
2026-06-21 18:43:15 +0530

    Create a factory helper for SUT

--- FeedApp/EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ FeedApp/EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -17,7 +17,7 @@ class CodableFeedStore {
 class CodableFeedStoreTests: XCTestCase {
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for retrieve to finish")
         sut.retrieve() { result in
@@ -34,7 +34,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for retrieve to finish")
         sut.retrieve() { firstResult in
@@ -50,6 +50,12 @@ class CodableFeedStoreTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> CodableFeedStore {
+        return CodableFeedStore()
     }
     
 }
