20a190080636e1c83c054d8c1ecd0b2ba94562b1
Mukesh <kmnrddy4334@gmail.com>
2026-06-25 00:42:40 +0530

    Insert overrides previous inserted feed

--- FeedApp/EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ FeedApp/EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -40,7 +40,7 @@ public class CoreDataFeedStore: FeedStore {
         let context = self.context
         context.perform {
             do {
-                let managedCache = ManagedCache(context: context)
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
                 managedCache.feed = ManagedFeedImage.images(from: images, in: context)
                 
@@ -93,6 +93,14 @@ private extension NSManagedObjectModel {
 private class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
+    
+    static func newUniqueInstance(
+        in context: NSManagedObjectContext
+    ) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        
+        return ManagedCache(context: context)
+    }
     
     static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
         let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
