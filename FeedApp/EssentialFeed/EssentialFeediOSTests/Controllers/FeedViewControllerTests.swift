ec83b68fa72a909ee3a1629ed65b435053039385
Mukesh <kmnrddy4334@gmail.com>
2026-07-02 23:46:33 +0530

    Extract FeedViewController test helpers to a separate file

--- FeedApp/EssentialFeed/EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
+++ FeedApp/EssentialFeed/EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
@@ -376,75 +376,6 @@ final class FeedViewControllerTests: XCTestCase {
 
 }
 
-private extension FeedViewController {
-    
-    var isShowingLoadingIndicator: Bool {
-        refreshControl?.isRefreshing == true
-    }
-    
-    func simulateViewAppearance() {
-        if !isViewLoaded {
-            loadViewIfNeeded()
-            updateRefreshControlWithFake()
-        }
-        
-        beginAppearanceTransition(true, animated: false)
-        endAppearanceTransition()
-    }
-    
-    func updateRefreshControlWithFake() {
-        let fakeRefreshControl = FakeRefreshControl()
-        fakeRefreshControl.addTargets(from: refreshControl)
-        refreshControl = fakeRefreshControl
-    }
-    
-    func simulateUserInitiatedFeedReload() {
-        refreshControl?.simulatePullToRefresh()
-    }
-    
-    func numberOfRenderedFeedImageViews() -> Int {
-        tableView(tableView, numberOfRowsInSection: feedImagesSection)
-    }
-    
-    func feedImageView(at index: Int = 0) -> UITableViewCell? {
-        let ds = tableView.dataSource
-        let indexPath = IndexPath(row: index, section: feedImagesSection)
-        return ds?.tableView(tableView, cellForRowAt: indexPath)
-    }
-    
-    var feedImagesSection: Int {
-        0
-    }
-    
-    @discardableResult
-    func simulateFeedImageViewVisible(at index: Int = 0) -> FeedImageCell? {
-        return feedImageView(at: index) as? FeedImageCell
-    }
-    
-    func simulateFeedImageViewNotVisible(at index: Int = 0) {
-        let cell = simulateFeedImageViewVisible(at: index)
-        
-        let delegate = tableView.delegate
-        let indexPath = IndexPath(row: index, section: feedImagesSection)
-        delegate?.tableView?(tableView, didEndDisplaying: cell!, forRowAt: indexPath)
-    }
-    
-    func simulateFeedImageViewNearVisible(at row: Int = 0) {
-        let ds = tableView.prefetchDataSource
-        
-        let index = IndexPath(row: row, section: feedImagesSection)
-        ds?.tableView(tableView, prefetchRowsAt: [index])
-    }
-    
-    func simulateFeedImageViewNotNearVisible(at row: Int = 0) {
-        simulateFeedImageViewNearVisible(at: row)
-        
-        let ds = tableView.prefetchDataSource
-        let index = IndexPath(row: row, section: feedImagesSection)
-        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
-    }
-}
-
 private extension FeedImageCell {
     var locationText: String? {
         locationLabel.text
@@ -475,7 +406,7 @@ private extension FeedImageCell {
     }
 }
 
-private class FakeRefreshControl: UIRefreshControl {
+class FakeRefreshControl: UIRefreshControl {
     private var _isRefreshing: Bool = false
     
     override var isRefreshing: Bool {
---
+++ FeedApp/EssentialFeed/EssentialFeediOSTests/Controllers/Helpers/FeedViewController+TestHelpers.swift
@@ -0,0 +1,78 @@
+//
+//  FeedViewController+TestHelpers.swift
+//  EssentialFeed
+//
+//  Created by Mukesh Kondreddy on 02/07/26.
+//
+
+import UIKit
+import EssentialFeediOS
+
+extension FeedViewController {
+    
+    var isShowingLoadingIndicator: Bool {
+        refreshControl?.isRefreshing == true
+    }
+    
+    func simulateViewAppearance() {
+        if !isViewLoaded {
+            loadViewIfNeeded()
+            updateRefreshControlWithFake()
+        }
+        
+        beginAppearanceTransition(true, animated: false)
+        endAppearanceTransition()
+    }
+    
+    func updateRefreshControlWithFake() {
+        let fakeRefreshControl = FakeRefreshControl()
+        fakeRefreshControl.addTargets(from: refreshControl)
+        refreshControl = fakeRefreshControl
+    }
+    
+    func simulateUserInitiatedFeedReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
+    
+    func numberOfRenderedFeedImageViews() -> Int {
+        tableView(tableView, numberOfRowsInSection: feedImagesSection)
+    }
+    
+    func feedImageView(at index: Int = 0) -> UITableViewCell? {
+        let ds = tableView.dataSource
+        let indexPath = IndexPath(row: index, section: feedImagesSection)
+        return ds?.tableView(tableView, cellForRowAt: indexPath)
+    }
+    
+    var feedImagesSection: Int {
+        0
+    }
+    
+    @discardableResult
+    func simulateFeedImageViewVisible(at index: Int = 0) -> FeedImageCell? {
+        return feedImageView(at: index) as? FeedImageCell
+    }
+    
+    func simulateFeedImageViewNotVisible(at index: Int = 0) {
+        let cell = simulateFeedImageViewVisible(at: index)
+        
+        let delegate = tableView.delegate
+        let indexPath = IndexPath(row: index, section: feedImagesSection)
+        delegate?.tableView?(tableView, didEndDisplaying: cell!, forRowAt: indexPath)
+    }
+    
+    func simulateFeedImageViewNearVisible(at row: Int = 0) {
+        let ds = tableView.prefetchDataSource
+        
+        let index = IndexPath(row: row, section: feedImagesSection)
+        ds?.tableView(tableView, prefetchRowsAt: [index])
+    }
+    
+    func simulateFeedImageViewNotNearVisible(at row: Int = 0) {
+        simulateFeedImageViewNearVisible(at: row)
+        
+        let ds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: feedImagesSection)
+        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
+    }
+}
