bf2ff3fa94c991b25d74685fc7878dda9a621a5c
Mukesh <kmnrddy4334@gmail.com>
2026-07-02 23:56:13 +0530

    Extract custom assertions to a separate file

--- FeedApp/EssentialFeed/EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
+++ FeedApp/EssentialFeed/EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
@@ -256,42 +256,6 @@ final class FeedViewControllerTests: XCTestCase {
         return (sut, loader)
     }
     
-    private func assertThat(
-        _ sut: FeedViewController,
-        isRendering feed: [FeedImage],
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        guard sut.numberOfRenderedFeedImageViews() == feed.count else {
-            XCTFail("Expected \(feed.count) images, got \(sut.numberOfRenderedFeedImageViews()) instead", file: file, line: line)
-            return
-        }
-
-        feed.enumerated().forEach { index, image in
-            assertThat(sut, hasViewConfiguredFor: image, at: index, file: file, line: line)
-        }
-    }
-    
-    private func assertThat(
-        _ sut: FeedViewController,
-        hasViewConfiguredFor image: FeedImage,
-        at index: Int,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        let view = sut.feedImageView(at: index)
-        
-        guard let cell = view as? FeedImageCell else {
-            XCTFail("Expected \(FeedImageCell.self) instance, got \(String(describing: view)) instead", file: file, line: line)
-            return
-        }
-
-        let shouldLocationBeVisible = image.location != nil
-        XCTAssertEqual(cell.isShowingLocation, shouldLocationBeVisible, file: file, line: line)
-        XCTAssertEqual(cell.locationText, image.location, file: file, line: line)
-        XCTAssertEqual(cell.descriptionText, image.description, file: file, line: line)
-    }
-    
     private func makeImage(
         description: String? = nil,
         location: String? = nil,
---
+++ FeedApp/EssentialFeed/EssentialFeediOSTests/Controllers/Helpers/FeedViewControllerTests+Assertions.swift
@@ -0,0 +1,50 @@
+//
+//  FeedViewControllerTests+Assertions.swift
+//  EssentialFeed
+//
+//  Created by Mukesh Kondreddy on 02/07/26.
+//
+
+import XCTest
+import EssentialFeed
+import EssentialFeediOS
+
+extension FeedViewControllerTests {
+    
+    func assertThat(
+        _ sut: FeedViewController,
+        isRendering feed: [FeedImage],
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        guard sut.numberOfRenderedFeedImageViews() == feed.count else {
+            XCTFail("Expected \(feed.count) images, got \(sut.numberOfRenderedFeedImageViews()) instead", file: file, line: line)
+            return
+        }
+        
+        feed.enumerated().forEach { index, image in
+            assertThat(sut, hasViewConfiguredFor: image, at: index, file: file, line: line)
+        }
+    }
+    
+    func assertThat(
+        _ sut: FeedViewController,
+        hasViewConfiguredFor image: FeedImage,
+        at index: Int,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let view = sut.feedImageView(at: index)
+        
+        guard let cell = view as? FeedImageCell else {
+            XCTFail("Expected \(FeedImageCell.self) instance, got \(String(describing: view)) instead", file: file, line: line)
+            return
+        }
+        
+        let shouldLocationBeVisible = image.location != nil
+        XCTAssertEqual(cell.isShowingLocation, shouldLocationBeVisible, file: file, line: line)
+        XCTAssertEqual(cell.locationText, image.location, file: file, line: line)
+        XCTAssertEqual(cell.descriptionText, image.description, file: file, line: line)
+    }
+    
+}
