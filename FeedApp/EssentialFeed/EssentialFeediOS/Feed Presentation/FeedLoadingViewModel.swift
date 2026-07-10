bf14a29e83f3e6c4e180d702dda7f953ad3bc4c2
Mukesh <kmnrddy4334@gmail.com>
2026-07-11 01:44:19 +0530

    Move ViewModels to separate files.

---
+++ FeedApp/EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Mukesh Kondreddy on 11/07/26.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ FeedApp/EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Mukesh Kondreddy on 11/07/26.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
