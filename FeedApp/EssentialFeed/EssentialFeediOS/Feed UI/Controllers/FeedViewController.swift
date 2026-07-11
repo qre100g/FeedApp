07a391a5e15ea6364ae7b126a965af314367475a
Mukesh <kmnrddy4334@gmail.com>
2026-07-12 01:06:18 +0530

    Move main thread dispatch to the Presenter

--- FeedApp/EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ FeedApp/EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -34,13 +34,6 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     func display(_ viewModel: FeedLoadingViewModel) {
-        
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async { [weak self] in
-                self?.display(viewModel)
-            }
-        }
-        
         if viewModel.isLoading {
             refreshControl?.beginRefreshing()
         } else {
