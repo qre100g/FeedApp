1648886f34e32222fee58a6a4668f85c5b12b854
Mukesh <kmnrddy4334@gmail.com>
2026-07-25 00:07:18 +0530

    Move FeedErrorViewModel to a separate file.

---
+++ FeedApp/EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeed
+//
+//  Created by Mukesh Kondreddy on 25/07/26.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
