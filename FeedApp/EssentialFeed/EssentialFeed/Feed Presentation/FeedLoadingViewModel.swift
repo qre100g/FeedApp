2d5238e5671603a8827061231db2060a1f5cfaf1
Mukesh <kmnrddy4334@gmail.com>
2026-07-25 01:38:34 +0530

    Move FeedLoadingViewModel to separate file.

---
+++ FeedApp/EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Mukesh Kondreddy on 25/07/26.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
