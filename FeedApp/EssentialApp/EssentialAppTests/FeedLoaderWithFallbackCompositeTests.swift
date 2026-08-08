0606beadc61e998a543fc84b6c6619ffab8a8a0c
Mukesh <kmnrddy4334@gmail.com>
2026-08-08 12:39:36 +0530

    Extract system under test (SUT) to a factory helper method

--- FeedApp/EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ FeedApp/EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -25,10 +25,7 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = uniqueFeed()
         let fallbackFeed = uniqueFeed()
-        
-        let primaryLoader = LoaderStub(result: .success(primaryFeed))
-        let fallbackLoader = LoaderStub(result: .success(fallbackFeed))
-        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(fallbackFeed))
         
         let exp = expectation(description: "Wait for load completion")
         sut.load { receivedResult in
@@ -45,8 +42,33 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     
     // MARK: - Helpers
     
+    private func makeSUT(
+        primaryResult: FeedLoader.Result,
+        fallbackResult: FeedLoader.Result,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> FeedLoaderWithFallbackComposite {
+        let primaryLoader = LoaderStub(result: primaryResult)
+        let fallbackLoader = LoaderStub(result: fallbackResult)
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        trackMemoryLeaks(primaryLoader, file: file, line: line)
+        trackMemoryLeaks(fallbackLoader, file: file, line: line)
+        trackMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
     private func uniqueFeed() -> [FeedImage] {
         return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "https://any-url.com")!)]
+    }
+    
+    private func trackMemoryLeaks(
+        _ instance: AnyObject,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should be deallocated", file: file, line: line)
+        }
     }
     
     private class LoaderStub: FeedLoader {
