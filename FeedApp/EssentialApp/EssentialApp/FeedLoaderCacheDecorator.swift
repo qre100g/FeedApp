34a559912b0c265600f2dded7d66d07953192619
Mukesh <kmnrddy4334@gmail.com>
2026-08-08 18:33:25 +0530

    Move FeedLoaderCacheDecorator to production

---
+++ FeedApp/EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,27 @@
+//
+//  FeedLoaderCacheDecorator.swift
+//  EssentialApp
+//
+//  Created by Mukesh Kondreddy on 08/08/26.
+//
+
+import EssentialFeed
+
+public class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    private let cache: FeedCache
+    
+    public init(decoratee: FeedLoader, cache: FeedCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            completion(result.map { feed in
+                self?.cache.save(feed) { _ in }
+                return feed
+            })
+        }
+    }
+}
