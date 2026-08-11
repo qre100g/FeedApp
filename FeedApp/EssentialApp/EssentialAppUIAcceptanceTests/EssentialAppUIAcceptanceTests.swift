f90759f0cdb42fcfe6702638e9cec82e212949cd
Mukesh <kmnrddy4334@gmail.com>
2026-08-12 00:09:25 +0530

    Add UI test target for running high level acceptance tests

---
+++ FeedApp/EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Mukesh Kondreddy on 11/08/26.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+
+    
+    
+}
