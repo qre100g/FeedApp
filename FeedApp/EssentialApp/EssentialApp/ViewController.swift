9fa23ecdfe6adaf158990053002ea8028733a4c6
Mukesh <kmnrddy4334@gmail.com>
2026-08-12 01:09:43 +0530

    Displayed cached feed on launch when customre has no connectivity

--- FeedApp/EssentialApp/EssentialApp/ViewController.swift
+++ FeedApp/EssentialApp/EssentialApp/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Mukesh Kondreddy on 06/08/26.
 //
 
+import CoreData
 import UIKit
 import EssentialFeed
 import EssentialFeediOS
@@ -17,20 +18,57 @@ class ViewController: UIViewController {
         
         let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
 
-        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let remoteClient = makeRemoteClient()
         let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
         let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+        
+        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appending(path: "feed-store.sqlite")
+        
+        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
                 
         let feedViewController = FeedUIComposer.feedComposedWith(
-            feedLoader: remoteFeedLoader,
-            imageLoader: remoteImageLoader)
+            feedLoader: FeedLoaderWithFallbackComposite(
+                primary: FeedLoaderCacheDecorator(
+                    decoratee: remoteFeedLoader,
+                    cache: localFeedLoader
+                ),
+                fallback: localFeedLoader
+            ),
+            imageLoader: FeedImageDataLoaderWithFallbackComposite(
+                primary: localImageLoader,
+                fallback: FeedImageDataLoaderCacheDecorator(
+                    decoratee: remoteImageLoader,
+                    cache: localImageLoader
+                )
+            )
+        )
         
         self.addChild(feedViewController)
         feedViewController.didMove(toParent: self)
         self.view.addSubview(feedViewController.view)
         
     }
+    
+    private func makeRemoteClient() -> HTTPClient {
+        switch UserDefaults.standard.string(forKey: "connectivity") {
+        case "offline":
+            AlwaysFailingHTTPClient()
+        default:
+            URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        }
+    }
+}
 
-
+private class AlwaysFailingHTTPClient: HTTPClient {
+    private class Task: HTTPClientTask {
+        func cancel() {}
+    }
+    
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> any HTTPClientTask {
+        completion(.failure(NSError(domain: "offline", code: 0)))
+        return Task()
+    }
 }
 
