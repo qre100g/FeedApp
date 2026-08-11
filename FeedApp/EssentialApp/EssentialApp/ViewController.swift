af3b209cdccf731f6eacec16aa041f9196b225cc
Mukesh <kmnrddy4334@gmail.com>
2026-08-12 00:49:36 +0530

    Displays remote feed on launch when customer has connectivity

--- FeedApp/EssentialApp/EssentialApp/ViewController.swift
+++ FeedApp/EssentialApp/EssentialApp/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import EssentialFeed
+import EssentialFeediOS
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+
+        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+                
+        let feedViewController = FeedUIComposer.feedComposedWith(
+            feedLoader: remoteFeedLoader,
+            imageLoader: remoteImageLoader)
+        
+        self.addChild(feedViewController)
+        feedViewController.didMove(toParent: self)
+        self.view.addSubview(feedViewController.view)
+        
     }
 
 
